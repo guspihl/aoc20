ce1a7e9b2afa6b8f25c5be1e8d0b67055c274f73
guspihl <guspihl@student.chalmers.se>
2020-12-12 03:30:49 +0100

    very slow but whatever

--- day11/day11_2.py
+++ day11/day11_2.py
@@ -2,34 +2,42 @@
 
 import itertools
 
-def check_empty_adjacents(i,j):
-    permutations = itertools.product([i-1,i,i+1],[j-1,j,j+1])
-    for x,y in permutations:
-        if x == i and y == j:
+def check_empty_adjacents(a,b):
+    directions = itertools.product(["i-1","i","i+1"],["j-1","j","j+1"])
+    for x,y in directions:
+        if x == "i" and y == "j":
             continue
-        if x < 0 or x >= len(l) or y < 0 or y >= len(l[x]):
-            continue
-        if l[x][y] == '#':
-            return False
+        i = a
+        j = b
+        i = eval(x)
+        j = eval(y)
+        while i >= 0 and i < len(l) and j >= 0 and j < len(l[i]):
+            if l[i][j] == 'L':
+                break
+            elif l[i][j] == '#':
+                return False
+            i = eval(x)
+            j = eval(y)
     return True
 
-def check_occupied_adjacents(i,j):
-    permutations = itertools.product([i-1,i,i+1],[j-1,j,j+1])
+def check_occupied_adjacents(a,b):
+    directions = itertools.product(["i-1","i","i+1"],["j-1","j","j+1"])
     n_occupied = 0
-    n_empty = 0
-    #check i+1 j+1 until out of bounds
-    for x,y in permutations:
-        if x == i and y == j:
+    for x,y in directions:
+        if x == "i" and y == "j":
             continue
-        if x < 0 or x >= len(l) or y < 0 or y >= len(l[x]):
-            n_empty += 1
-            continue
-        if l[x][y] == '#':
-            n_occupied += 1
-        else:
-            n_empty += 1
-        if n_occupied >= 5 or n_empty >= 4:
-            break
+        i = a
+        j = b
+        i = eval(x)
+        j = eval(y)
+        while i >= 0 and i < len(l) and j >= 0 and j < len(l[i]):
+            if l[i][j] == 'L':
+                break
+            elif l[i][j] == '#':
+                n_occupied += 1
+                break
+            i = eval(x)
+            j = eval(y)
     return True if n_occupied >= 5 else False
 
 l = []
@@ -38,7 +46,11 @@ for i in file.readlines():
     i = i.replace('\n','')
     l.append(i)
 
+k=0
 while True:
+    print(k)
+    for i in l:
+        print(i)
     new_world = []
     state_changed = False
     for i in range(len(l)):
@@ -62,15 +74,11 @@ while True:
     if not state_changed:
         break
     l = new_world
+    k+=1
 
 #Count number of occupied seats
 occupied_seats = 0
 for i in l:
     occupied_seats += i.count('#')
 
-print("Part 1:", occupied_seats)
-
-
-permutations = itertools.product(["i-1","i","i+1"],["j-1","j","j+1"])
-a = list(permutations).remove(4)
-print(a)
+print("Part 2:", occupied_seats)
